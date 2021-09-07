b85a8001bac38861c5b723890023d43ed68ce82c
brunocoronado49 <jofrancoracor@gmail.com>
2021-09-06 19:02:30 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Bruno Coronado on 06/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
